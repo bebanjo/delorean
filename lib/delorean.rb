1979ed2a22c4e9ce6fe3a455517bbc715886df1d
retr0h <john@dewey.ws>
2011-02-28 11:36:25 -0800

    Added back_to_1985 and removed trailing whitespace

--- lib/delorean.rb
+++ lib/delorean.rb
@@ -2,7 +2,7 @@ require 'chronic'
 
 module Delorean
   extend self
-    
+
   def time_travel_to(time, options={})
     mock_current_time(time, options)
     return unless block_given?
@@ -14,34 +14,35 @@ module Delorean
   end
 
   def back_to_the_present
-    reset  
+    reset
   end
+  alias :back_to_1985 :back_to_the_present
 
   def jump(seconds)
     mock_current_time Time.now + seconds
   end
-  
+
   def now
     Time.now_without_delorean - time_travel_offsets.inject(0){ |sum, val| sum + val }
   end
-  
+
   private
-  
+
   def time_travel_offsets
     @@time_travel_offsets ||= []
   end
-  
+
   def reset
     @@time_travel_offsets = []
   end
-  
+
   def mock_current_time(time, options={})
     time = Chronic.parse(time, options) if time.is_a?(String)
     time = Time.local(time.year, time.month, time.day) if time.is_a?(Date)
-    
+
     time_travel_offsets.push Time.now - time
   end
-  
+
   def restore_previous_time
     time_travel_offsets.pop
   end
--- spec/delorean_spec.rb
+++ spec/delorean_spec.rb
@@ -3,25 +3,25 @@ require 'active_support/all'
 require File.dirname(__FILE__) + "/../lib/delorean"
 
 describe Delorean do
-  
+
   after(:each) do
     Delorean.back_to_the_present
   end
-  
+
   describe "time_travel_to" do
     it "should travel through time" do
       past_date = Time.utc(2009,1,1,10,30)
       Delorean.time_travel_to past_date
       Time.now.should be_close(past_date, 1)
     end
-    
+
     it "should travel through time several times" do
       past_date = Date.new(2009,1,1)
       Delorean.time_travel_to Date.new(2009,2,20)
       Delorean.time_travel_to past_date
       Date.today.should == past_date
     end
-    
+
     it "should travel to string times" do
       two_minutes_from_now = Time.now + 120
       Delorean.time_travel_to "2 minutes from now"
@@ -42,21 +42,21 @@ describe Delorean do
       end.should eql 'Back in good old 1955.'
     end
   end
-  
+
   describe "back_to_the_present" do
     it "should stay in the present if in the present" do
       today = Date.today
       Delorean.back_to_the_present
       Date.today.should == today
     end
-    
+
     it "should go back to the present if not in the present" do
       today = Date.today
       Delorean.time_travel_to Time.local(2009,2,2,1,1)
       Delorean.back_to_the_present
       Date.today.should == today
     end
-    
+
     it "should go back to the original present if travelled through time several times" do
       today = Date.today
       2.times { Delorean.time_travel_to Time.local(2009,2,2,1,1) }
@@ -64,7 +64,7 @@ describe Delorean do
       Date.today.should == today
     end
   end
-  
+
   describe "time_travel_to with block" do
     it "should travel through time" do
       past_date = Time.utc(2009,1,1,10,30)
@@ -72,13 +72,13 @@ describe Delorean do
         Time.now.should be_close(past_date, 1)
       end
     end
-    
+
     it "should return to the future" do
       today = Date.today
       Delorean.time_travel_to(Time.utc(2009,2,2,10,30)) {}
       Date.today.should == today
     end
-    
+
     it "should travel through time several times" do
       Delorean.time_travel_to(Time.utc(2009,2,2,10,40)) do
         Delorean.time_travel_to(Time.utc(2009,1,1,22,45)) do
@@ -86,7 +86,7 @@ describe Delorean do
         end
       end
     end
-    
+
     it "should still return to the future" do
       today = Date.today
       Delorean.time_travel_to(Time.utc(2009,2,2,10,40)) do
@@ -95,7 +95,7 @@ describe Delorean do
       end
       Date.today.should == today
     end
-    
+
     it "should travel to string times" do
       two_minutes_ago = Time.now - 120
       Delorean.time_travel_to("2 minutes ago") do
