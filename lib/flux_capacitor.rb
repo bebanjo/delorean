9b8c694017c63735a2fe271f5edebd7708f236fb
alejanderl <janderl@gmail.com>
2014-04-10 13:44:23 +0200

    Added freeze time methods
    Added freezing time
     methods
    
    mend

--- lib/flux_capacitor.rb
+++ lib/flux_capacitor.rb
@@ -26,10 +26,19 @@ module Delorean
     end
 
     def now
+      return frozen_time if frozen_time
       Time.now_without_delorean - time_travel_offsets.inject(0){ |sum, val| sum + val }
     end
 
+    def freeze_time(time = Time.now, options={})      
+      mock_current_time time, options
+      @@frozen_time =  Time.now
+    end
+
     private
+    def frozen_time
+      @@frozen_time ||= false
+    end
 
     def time_travel_offsets
       @@time_travel_offsets ||= []
@@ -37,17 +46,23 @@ module Delorean
 
     def reset
       @@time_travel_offsets = []
+      unfreeze_time
     end
 
-    def mock_current_time(time, options={})
+    def mock_current_time(time, options={})      
       time = Chronic.parse(time, options) if time.is_a?(String)
       time = Time.local(time.year, time.month, time.day) if time.is_a?(Date) && !time.is_a?(DateTime)
-
+      unfreeze_time
       time_travel_offsets.push Time.now - time
     end
 
     def restore_previous_time
       time_travel_offsets.pop
     end
+
+    def unfreeze_time
+      @@frozen_time = nil
+    end
   end
 end
+
